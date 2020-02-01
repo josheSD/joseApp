768c54c5ec450969938dc1c6aa341abb3a82e18c
jose <jose@gmail.com>
2020-02-01 10:49:52 -0500

    camara

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'joseApp';
 
+  ngOnInit(){
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition((position)=>{
+        console.log('latitud => '+position.coords.latitude)
+        console.log('longitud => '+position.coords.longitude)
+      });
+    }else{
+      console.log('NO esta soportado la geolocalización');
+    }
+  }
+  
   chatWhatsApp(){
     let url =  "https://api.whatsapp.com/send?phone=51953235885&text=Hola tengo algunas preguntas";
     window.open(url,"_blank");
   }
+
+  
 }
